f4df444a5608ccb3c17fb78c18b8de83627428bd
Edgar <edgar7sagan@hotmail.com>
2017-09-07 20:56:24 -0500

    Update ContactoAdaptador.java
    Con código para manejar el botón like.

--- app/src/main/java/com/solrom/edgar/miscontactos/ContactoAdaptador.java
+++ app/src/main/java/com/solrom/edgar/miscontactos/ContactoAdaptador.java
@@ -1,14 +1,20 @@
 package com.solrom.edgar.miscontactos;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.view.menu.MenuView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static com.solrom.edgar.miscontactos.R.id.rvContactos;
 
 /**
  * Created by edgarsr on 05/09/17.
@@ -17,9 +23,12 @@ import java.util.ArrayList;
 public class ContactoAdaptador extends RecyclerView.Adapter<ContactoAdaptador.ContactoViewHolder>{
 
     ArrayList<Contacto> contactos;
+    Activity activity;
 
-    public  ContactoAdaptador(ArrayList<Contacto> contactos){// construye la lista de contactos
+    public  ContactoAdaptador(ArrayList<Contacto> contactos, Activity activity){// construye la lista de contactos
         this.contactos = contactos;
+        this.activity = activity;
+
     }
 
 
@@ -32,10 +41,27 @@ public class ContactoAdaptador extends RecyclerView.Adapter<ContactoAdaptador.Co
     @Override
     public void onBindViewHolder(ContactoViewHolder contactoViewHolder, int position) {
         //vamos a pasar la lista de contactos para setear los valores de los objetos de la lista de contactos, va iterando
-        Contacto contacto = contactos.get(position);
+        final Contacto contacto = contactos.get(position);
         contactoViewHolder.imgFoto.setImageResource(contacto.getFoto());
         contactoViewHolder.tvNombreCV.setText(contacto.getNombre());
         contactoViewHolder.tvTelefonoCV.setText(contacto.getTelefono());
+        contactoViewHolder.imgFoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){//mensaje Toast que muestra nombre del contacto al dar click en la foto
+                Toast.makeText(activity, contacto.getNombre(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(activity, DetalleContacto.class);//colocamos el intent para que nos lleve a la actividad DetalleContacto
+                intent.putExtra("nombre", contacto.getNombre());
+                intent.putExtra("telefono", contacto.getTelefono());
+                intent.putExtra("email", contacto.getEmail());
+                activity.startActivity(intent);
+            }
+        });
+        contactoViewHolder.btnLike.setOnClickListener(new View.OnClickListener(){// al tocar al boton de like va a aparecer un toast
+            @Override
+            public void onClick(View v){
+                Toast.makeText(activity, "Diste like a " + contacto.getNombre(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
     }
@@ -50,6 +76,7 @@ public class ContactoAdaptador extends RecyclerView.Adapter<ContactoAdaptador.Co
         private ImageView imgFoto;
         private TextView tvNombreCV;
         private TextView tvTelefonoCV;
+        private ImageButton btnLike;
 
 
         public ContactoViewHolder(View itemView) {
@@ -57,6 +84,7 @@ public class ContactoAdaptador extends RecyclerView.Adapter<ContactoAdaptador.Co
             imgFoto = (ImageView) itemView.findViewById(R.id.imgFoto);
             tvNombreCV = (TextView) itemView.findViewById(R.id.tvNombreCV);
             tvTelefonoCV = (TextView) itemView.findViewById(R.id.tvTelefonoCV);
+            btnLike = (ImageButton) itemView.findViewById(R.id.btnLike);
         }
     }
 }
