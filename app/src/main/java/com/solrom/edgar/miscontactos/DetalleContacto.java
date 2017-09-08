d544d732f28651ba51efed784c6da6b25664fb59
Edgar <edgar7sagan@hotmail.com>
2017-09-07 20:57:44 -0500

    Update DetalleContacto.java

--- app/src/main/java/com/solrom/edgar/miscontactos/DetalleContacto.java
+++ app/src/main/java/com/solrom/edgar/miscontactos/DetalleContacto.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -21,6 +22,8 @@ public class DetalleContacto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalle_contacto);
+        Toolbar miActionBar = (Toolbar) findViewById(R.id.miActionBar);
+        setSupportActionBar(miActionBar);
 
         Bundle parametros = getIntent().getExtras();
         String nombre = parametros.getString(getResources().getString(R.string.pnombre));
@@ -72,3 +75,4 @@ public class DetalleContacto extends AppCompatActivity {
         return super.onKeyDown(keyCode, event);
     }
 }
+
